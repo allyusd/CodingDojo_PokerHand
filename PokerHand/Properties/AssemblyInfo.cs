dc375af03e975ec2f7cde3c57de2e0b20b7e2487
allyusd <allyusd@yahoo.com.tw>
2019-01-22 22:23:18 +0800

    init: init project

---
+++ PokerHand/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PokerHand")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Toshiba")]
+[assembly: AssemblyProduct("PokerHand")]
+[assembly: AssemblyCopyright("Copyright © Toshiba 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f97d52bd-9487-41f9-a21c-6e49bffef1a3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
