a2c44e65871b65bc3be365ebf90b97b0d48368c9
allyusd <allyusd@yahoo.com.tw>
2019-01-26 20:33:27 +0800

    test: SpecialPoint

--- PokerHand/CardConverterToolTest.cs
+++ PokerHand/CardConverterToolTest.cs
@@ -33,6 +33,20 @@ namespace PokerHand
             CardShouldBe(CardSuit.Spade, 6, cards[4]);
         }
 
+        [TestMethod]
+        public void SpecialPoint()
+        {
+            var convert = new CardConverterTool();
+            var cards = convert.ConvertStringToCards("H10,HJ,HQ,HK,HA");
+
+            Assert.AreEqual(5, cards.Count);
+            CardShouldBe(CardSuit.Heart, 10, cards[0]);
+            CardShouldBe(CardSuit.Heart, 11, cards[1]);
+            CardShouldBe(CardSuit.Heart, 12, cards[2]);
+            CardShouldBe(CardSuit.Heart, 13, cards[3]);
+            CardShouldBe(CardSuit.Heart, 14, cards[4]);
+        }
+
         private static void CardShouldBe(CardSuit suit, int point, Card card)
         {
             Assert.AreEqual(suit, card.suit);
