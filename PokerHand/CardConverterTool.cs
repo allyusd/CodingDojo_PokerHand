5173413551df85ce7300a18971c204bd6306c926
allyusd <allyusd@yahoo.com.tw>
2019-01-26 20:39:18 +0800

    feat: SpecialPoint

--- PokerHand/CardConverterTool.cs
+++ PokerHand/CardConverterTool.cs
@@ -13,6 +13,15 @@ namespace PokerHand
             {"C", CardSuit.Club}
         };
 
+        private readonly Dictionary<string, int> _pointLookup = new Dictionary<string, int>()
+        {
+            {"J", 11},
+            {"Q", 12},
+            {"K", 13},
+            {"A", 14}
+        };
+
+
         public List<Card> ConvertStringToCards(string cardsInput)
         {
             var cards = new List<Card>();
@@ -24,12 +33,18 @@ namespace PokerHand
                 var card = new Card();
 
                 var suit = str.Substring(0, 1);
-
                 card.suit = _suitLookup[suit];
 
                 var point = str.Substring(1, str.Length - 1);
 
-                card.point = Convert.ToInt32(point);
+                try
+                {
+                    card.point = Convert.ToInt32(point);
+                }
+                catch (FormatException)
+                {
+                    card.point = _pointLookup[point];
+                }
 
                 cards.Add(card);
             }
