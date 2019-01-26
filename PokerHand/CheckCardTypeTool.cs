249f177198a66a7db64b96874000fbf0aaf6e40e
allyusd <allyusd@yahoo.com.tw>
2019-01-26 21:18:05 +0800

    feat: CheckStraight

--- PokerHand/CheckCardTypeTool.cs
+++ PokerHand/CheckCardTypeTool.cs
@@ -13,7 +13,19 @@ namespace PokerHand
                 return;
             }
 
+            if (IsStraight(cards))
+            {
+                CardType = CardType.Straight;
+                return;
+            }
+
             CardType = CardType.HighCard;
+        }
+
+        private static bool IsStraight(List<Card> cards)
+        {
+            return (cards.GroupBy(c => c.point).Count() == 5)
+                   && (cards.Max(c => c.point) - cards.Min(c => c.point) == 4);
         }
 
         private static bool IsFlush(List<Card> cards)
