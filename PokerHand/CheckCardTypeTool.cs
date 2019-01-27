e55ab7859ff49df7379dddad1e01908b7b0282a8
allyusd <allyusd@yahoo.com.tw>
2019-01-27 15:34:36 +0800

    feat: CheckSpecialStraight

--- PokerHand/CheckCardTypeTool.cs
+++ PokerHand/CheckCardTypeTool.cs
@@ -13,7 +13,7 @@ namespace PokerHand
                 return;
             }
 
-            if (IsStraight(cards))
+            if (IsStraight(cards) || IsSpecialStraight(cards))
             {
                 CardType = CardType.Straight;
                 return;
@@ -22,10 +22,24 @@ namespace PokerHand
             CardType = CardType.HighCard;
         }
 
+        private static bool IsSpecialStraight(List<Card> cards)
+        {
+            return CardsHasOncePointCard(cards, 14)
+                && CardsHasOncePointCard(cards, 2)
+                && CardsHasOncePointCard(cards, 3)
+                && CardsHasOncePointCard(cards, 4)
+                && CardsHasOncePointCard(cards, 5);
+        }
+
+        private static bool CardsHasOncePointCard(List<Card> cards, int point)
+        {
+            return cards.Count(c => c.point == point) == 1;
+        }
+
         private static bool IsStraight(List<Card> cards)
         {
             return (cards.GroupBy(c => c.point).Count() == 5)
-                   && (cards.Max(c => c.point) - cards.Min(c => c.point) == 4);
+                && (cards.Max(c => c.point) - cards.Min(c => c.point) == 4);
         }
 
         private static bool IsFlush(List<Card> cards)
